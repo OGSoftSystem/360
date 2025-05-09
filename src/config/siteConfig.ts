7e6f5fe8b5c83afa6a066109f7f0a2fb0bf29bb5
chukumela <chukumelawilson@gmail.com>
2025-05-09 11:54:04 +0100

    author detail

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -4,5 +4,5 @@ export const siteConfig = {
     "A Full-Service Consultancy Firm Dedicated To Supporting The Growth And Success Of Tht Gaming Industry.",
   shortDescription: "A Full-Service Consultancy Firm",
   baseUrl: "https://resolution360.net",
-  author: { name: "OSoft Solutions", url: "https://ogsoftsolutions.com" },
+  author: { name: "OSoft Solutions Ltd.", url: "https://ogsoftsolutions.com" },
 };
